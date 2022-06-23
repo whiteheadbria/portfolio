49bdfcca8bddaac935fa69b4b398433685cdecd2
Bria Whitehead <bria.whitehead@ibm.com>
2022-06-23 15:52:56 -0500

    edited stuff. lots

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 function openNav() {
   document.getElementById("sidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
+  
 }
 
 function closeNav() {
   document.getElementById("sidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
+ 
 }
